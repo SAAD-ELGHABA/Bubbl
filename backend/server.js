b2c39b62719b8251924604c6f4868153593b276c
SAAD-ELGHABA <saadlraba@gmail.com>
2025-09-01 12:14:15 +0100

    initializing Bubbl

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+dotenv.config();
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error(err));
+
+
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
