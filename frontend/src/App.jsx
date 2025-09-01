297b5c3dcf486fd8bc95f3f5a6a9ab5aa8d8149d
SAAD-ELGHABA <saadlraba@gmail.com>
2025-09-01 12:58:46 +0100

    some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
-
+import React from "react";
 
 function App() {
 
-  return <div>ffff</div>;
+  return <div className="text-[#34a0a4] flex items-center justify-center h-screen font-black text-5xl">Bubbl</div>;
 }
 
 export default App;
