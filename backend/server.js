10854896a2f2339a483e67922c055e7447c2ea93
SAAD-ELGHABA <saadlraba@gmail.com>
2025-09-09 15:37:24 +0100

    cors settings

--- backend/server.js
+++ backend/server.js
@@ -71,6 +71,8 @@ io.on("connection", (socket) => {
 
 mongoose
   .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     serverSelectionTimeoutMS: 5000,
   })
   .then(() => console.log("✅ MongoDB connected"))
