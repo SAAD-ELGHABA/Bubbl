06545ecc5d97b59efdf9baa89152d61338cf9ffe
SAAD-ELGHABA <saadlraba@gmail.com>
2025-09-09 15:17:52 +0100

    some changes

--- backend/server.js
+++ backend/server.js
@@ -18,12 +18,12 @@ const app = express();
 const server = createServer(app);
 
 const io = new Server(server, {
-  cors: { origin: "*", methods: ["GET", "POST"] },
+  cors: { origin: [process.env.FRONTEND_URL, "http://localhost:3000","http://localhost:5173"], methods: ["GET", "POST"] },
 });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000","http://localhost:5173"],
     credentials: true,
   })
 );
